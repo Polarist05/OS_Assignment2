fc0b3f427ccb9210a9fbedfcb555b4e1bdffe568
Polarist05 <88574175+Polarist05@users.noreply.github.com>
2023-09-18 19:14:56 +0700

    Multi thread ver.1
    use lock to make only one thread can enqueue and dequeue(seperate enqueue&dequeue)

--- OS_Assignment2/Program.cs
+++ OS_Assignment2/Program.cs
@@ -7,12 +7,33 @@ class Thread_safe_buffer
     static int Front = 0;
     static int Back = 0;
     static int Count = 0;
+    static bool enQueueFlag = false;
+    static bool deQueueFlag = false;
+    static object enqueueObj = new object();
+    static object dequeueObj = new object();
 
     static void EnQueue(int eq)
     {
+        while (true)
+        {
+
+            while (enQueueFlag)
+            { 
+                Thread.Sleep(10);
+            }
+            lock (enqueueObj)
+            {
+                if (!enQueueFlag)
+                {
+                    enQueueFlag = true;
+                    break;
+                }
+                
+            }
+        }
+
         if (Count + 1 >= Capacity)
         {
-            //Console.WriteLine("UPDATE");
             int newCapacity = Capacity * 2;
             var newBuffer = new int[newCapacity];
             for (int i = 0, j = Front; j != Back; i++) 
@@ -25,24 +46,43 @@ class Thread_safe_buffer
             Capacity = newCapacity;
             TSBuffer = newBuffer;
         }
-        //Console.WriteLine($"Back :{Back} {TSBuffer.Length}");
         TSBuffer[Back] = eq;
         Back = (++Back >= Capacity) ? 0 : Back;
         Count += 1;
+        enQueueFlag = false;
     }
 
     static int DeQueue()
     {
+        while (true)
+        {
+
+            while (deQueueFlag)
+            {
+                Thread.Sleep(10);
+            }
+            lock (dequeueObj)
+            {
+                if (!deQueueFlag)
+                {
+                    deQueueFlag = true;
+                    break;
+                }
+
+            }
+        }
         int x = 0;
         x = TSBuffer[Front];
         if(Count > 0)
         {
             Front = ++Front==Capacity?0:Front;
             Count -= 1;
+            deQueueFlag = false;
             return x;
         }
         else
         {
+            deQueueFlag = false;
             //ADD EXCEPTION OR SOMETHING
             return 0;
         }
@@ -86,17 +126,18 @@ class Thread_safe_buffer
     static void Main(string[] args)
     {
         Thread t1 = new Thread(th01);
-        //Thread t11 = new Thread(th011);
+        Thread t11 = new Thread(th011);
         Thread t2 = new Thread(th02);
-        //Thread t21 = new Thread(th02);
-        //Thread t22 = new Thread(th02);
+        Thread t21 = new Thread(th02);
+        Thread t22 = new Thread(th02);
 
         t1.Start();
-        //t11.Start();
+        t11.Start();
         t1.Join();
+        t11.Join();
         t2.Start(1);
-        //t21.Start(2);
-        //t22.Start(3);
+        t21.Start(2);
+        t22.Start(3);
     }
 }
 
